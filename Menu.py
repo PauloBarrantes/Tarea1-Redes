b7167f7eb25cb88b05938e589ca2d2f513d4aeca
Paulo Barrantes <paulobarrantes@MacBook-Pro-de-Paulo.local>
2018-09-08 10:14:46 -0600

    Patada Inicial

---
+++ Menu.py
@@ -0,0 +1,25 @@
+
+## Imports
+from Node import *
+## Colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+## Menu
+
+print(bcolors.WARNING+"Welcome!" +bcolors.ENDC)
+print("What do you want to do?")
+print("1 - Create a node ")
+print("2 - Delete a node ")
+print("3 - Communicate nodes")
+
+opcion = input("¿? \n")
+print(opcion)
+
+Node('localhost',8000)
---
+++ ReachabilityTables.py
@@ -0,0 +1,4 @@
+class ReachabilityTables():
+    """docstring for ReachabilityTables."""
+    def __init__(self, arg):
+        self.arg = arg
