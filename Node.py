16643ea8864e01dcf638d2a8a2020dd24fb1c162
aflaster11 <a.flaster@gmail.com>
2018-09-18 10:12:33 -0600

    nodeMenu

--- Node.py
+++ Node.py
@@ -1,4 +1,14 @@
 import socket
+## Colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Node:
     def __init__(self, protocol, ipNode, port):
@@ -9,8 +19,25 @@ class Node:
     def __del__(self):
         print("Node was successfully deleted")
 
-    def broadcastClosure():
-        #send messages
-        pass
+    def nodeMenu(self):
+        ##Variables
+        opcion = "0"
+        ## Menu
+        while opcion != "3":
+            print(bcolors.WARNING+"You are now working with node", ip + "!" +bcolors.ENDC)
+            print("What do you want to do?")
+            print("1 - Enviar Mensaje ")
+            print("2 - Borrar nodo ")
+            print("3 - Salir")
 
+            opcion = input("¿? \n")
+
+            if(opcion ==  "1"):
+                print("Usted quiere enviar un mensaje")
+            if(opcion == "2"):
+                print("Usted quiere eliminar este nodo")
+                #envia mensaje con un 0 para que se borre de las tablas
+                #del self
+            if(opcion == "3"):
+                print("Good bye!")
 
--- pruebas.py
+++ pruebas.py
@@ -7,6 +7,28 @@ class Node:
     def __del__(self):
         print("Node was successfully deleted")
 
+    def nodeMenu(self):
+        ##Variables
+        opcion = "0"
+        ## Menu
+        while opcion != "3":
+            print("You are now working with node", self.direccionNodo + "!")
+            print("What do you want to do?")
+            print("1 - Enviar Mensaje ")
+            print("2 - Borrar nodo ")
+            print("3 - Salir")
+
+            opcion = input("¿? \n")
+
+            if(opcion ==  "1"):
+                print("Usted quiere enviar un mensaje")
+            if(opcion == "2"):
+                print("Usted quiere eliminar este nodo")
+                #envia mensaje con un 0 para que se borre de las tablas
+                #del self
+            if(opcion == "3"):
+                print("Good bye!")
+
 
 class NodeTCP(Node):
 
@@ -17,3 +39,4 @@ class NodeTCP(Node):
 
 nodo = NodeTCP("1.1.1.1", "900")
 print(nodo.protocolo, nodo.direccionNodo, nodo.puertoNodo, nodo.servidor)
+nodo.nodeMenu()
