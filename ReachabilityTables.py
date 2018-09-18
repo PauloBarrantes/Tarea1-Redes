8265a40eb00d26ef42eeecca16c62c5745de0a0e
PauloBarrantes <paulo.barrantes@ucr.ac.cr>
2018-09-18 15:14:36 -0600

    Tabla de alcanzabilidad

--- ReachabilityTables.py
+++ ReachabilityTables.py
@@ -1,12 +1,26 @@
+from texttable import *
+
 class ReachabilityTables():
     """docstring for ReachabilityTables."""
-    def __init__(self, arg):
+    def __init__(self):
         self.tabla = {'localhost':[1,24,100]}
 
     def agregarDireccion(self, ip, red, mascara, costo):
         self.tabla.update({ip:[red, mascara, costo]})
     def imprimirTabla(self):
-        print(self.tabla)
+        print("TABLA DE ALCANZABILIDAD")
+        table = Texttable()
+        table.set_cols_align(["l", "r", "c","k"])
+        table.set_cols_valign(["t", "m", "b","a"])
+        table.add_rows([
+                        ["IP", "Red", "Máscara","Costo"],
+                        for key in self.tabla:
+                            [value.key]
+                            ["GG", 32, "Xav'",334],
+                            ["GG", 32, "Xav'",334],
+                            ["GG", 2, "F'",1]])
+        print (table.draw() + "\n")
+
     def buscarDireccion (self, ip):
         result = self.tabla.get(ip)
         if result :
@@ -16,3 +30,9 @@ class ReachabilityTables():
 
 
 gg = ReachabilityTables()
+gg.imprimirTabla()
+gg.agregarDireccion("172.18.190.1",3,4,599)
+gg.imprimirTabla()
+
+dir = gg.buscarDireccion("localhost")
+print(dir)
