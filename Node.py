313f5bba46c7f68c71126c4dd4f1084ee770ad93
PauloBarrantes <paulo.barrantes@ucr.ac.cr>
2018-09-20 14:15:42 -0600

    gg

--- Node.py
+++ Node.py
@@ -20,24 +20,4 @@ class Node:
     def __del__(self):
         print("Node was successfully deleted")
 
-    def nodeMenu(self):
-        ##Variables
-        opcion = "0"
-        ## Menu
-        while opcion != "3":
-            print(bcolors.WARNING+"You are now working with node", ip + "!" +bcolors.ENDC)
-            print("What do you want to do?")
-            print("1 - Enviar Mensaje ")
-            print("2 - Borrar nodo ")
-            print("3 - Salir")
-
-            opcion = input("¿? \n")
-
-            if(opcion ==  "1"):
-                print("Usted quiere enviar un mensaje")
-            if(opcion == "2"):
-                print("Usted quiere eliminar este nodo")
-                #envia mensaje con un 0 para que se borre de las tablas
-                #del self
-            if(opcion == "3"):
-                print("Good bye!")
+    
