13d9623ba3f0296c414fed9780b6dd7a840757a5
PauloBarrantes <paulobarrante@gmail.com>
2018-11-13 09:09:52 -0600

    Nodo Central

---
+++ Fase3/CentralNode.py
@@ -0,0 +1,158 @@
+from Node import *
+from socket import *
+from texttable import *
+import csv
+'''
+    Central Node
+        IP: 127.0.0.1
+        Port: 9000
+'''
+
+
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    GG = '\033[96m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class CentralNode(Node):
+    def __init__(self, ip, port):
+        super().__init__("intAS", ip, int(port))
+
+        '''
+            Neighbors Table
+
+            0 -> IP A
+            1 -> Mask A
+            2 -> Port A
+            3 -> IP B
+            4 -> Mask B
+            5 -> Port B
+            6 -> Cost between A and B
+        '''
+        self.neighbors = []
+        self.extract_neighbors()
+        # Start server thread.
+        self.threadServer = threading.Thread(target = self.server_central)
+
+        # Continue the server, even after our main thread dies.
+        self.threadServer.daemon = True
+        self.threadServer.start()
+
+        self.menu()
+
+
+
+    def server_central(self):
+
+        self.server_socket = socket(AF_INET, SOCK_DGRAM)
+        self.server_socket.bind((self.ip, self.port))
+        print ("El servidor central está listo: ", self.ip, self.port)
+
+        while True:
+            message, client_addr = self.server_socket.recvfrom(1024)
+            if int.from_bytes(message, byteorder="big") != 0:
+
+                self.log_writer.write_log("Central Node received a request.", 1)
+                neighborsList = []
+
+                ip_bytes = message[0:4]
+                mask = message[5]
+                port = message[6:8]
+
+
+                ip = list(ip_bytes)
+                ip_str = ""
+                for byte in range(0,len(ip)):
+                    if(byte < len(ip)-1):
+                        ip_str += str(ip[byte])+"."
+                    else:
+                        ip_str += str(ip[byte])
+                mask_str = str(mask)
+                cost = int.from_bytes(cost_bytes,byteorder="big")
+
+
+                ipRequest = "127.0.0.1"
+                maskRequest = 16
+                portRequest = 8080
+
+
+                for i in range (0,len(self.neighbors)):
+                    neighbor = []
+                    if neighbors[i][0] == ipRequest and int(neighbors[i][1]) == maskRequest and int(neighbors[i][2]):
+                        print("Vecino A - B")
+                        neighbor.append(neighbors[i][0])
+                        neighbor.append(neighbors[i][1])
+                        neighbor.append(neighbors[i][2])
+                    elif neighbors[i][3] == ipRequest and int(neighbors[i][4]) == maskRequest and int(neighbors[i][5]):
+                        print("Vecino B - A")
+                        neighbor.append(neighbors[i][3])
+                        neighbor.append(neighbors[i][4])
+                        neighbor.append(neighbors[i][5])
+            else:
+                print("Se han comunicado conmigo, pero no respetaron el protocolo :v")
+
+
+
+    def extract_neighbors(self):
+        with open('neighbors.csv', newline='') as csvfile:
+            # We obtain neighbor from csv file
+            neighborsCSV = csv.reader(csvfile, delimiter=';', quotechar='|')
+
+            # and fill a structure with each neighbor pair
+            for row in neighborsCSV:
+                neighbor = []
+                neighbor.append(row[0])
+                neighbor.append(row[1])
+                neighbor.append(row[2])
+                neighbor.append(row[3])
+                neighbor.append(row[4])
+                neighbor.append(row[5])
+                neighbor.append(row[6])
+                self.neighbors.append(neighbor)
+
+
+    def printNeighbors(self, neighborsList):
+        print("Lista de Vecinos")
+        table = Texttable()
+        table.set_cols_align(["c", "c","c","c", "c","c","c"])
+        table.set_cols_valign(["m", "m","m","m", "m","m","m"])
+        table.add_row([
+        "ip A",
+        "Máscara A",
+        "Puerto A",
+        "ip B",
+        "Máscara B",
+        "Puerto B",
+        "Costo "])
+
+        for i in range (0,len(neighborsList)):
+            ipA = neighborsList[i][0]
+            maskA = neighborsList[i][1]
+            portA = neighborsList[i][2]
+            ipB = neighborsList[i][3]
+            maskB = neighborsList[i][4]
+            portB = neighborsList[i][5]
+            cost = neighborsList[i][6]
+            table.add_row([ipA,maskA,portA,ipB, maskB, portB,cost])
+
+
+        print (table.draw() + "\n")
+    def menu(self):
+        print(BColors.WARNING + "Bienvenido al nodo central!: " + self.ip, ":", str(self.port) + BColors.ENDC)
+        print(BColors.OKGREEN + "Instrucciones: " + BColors.ENDC)
+        print(BColors.BOLD + "-1-" + BColors.ENDC, "Para acabar con la vida de este nodo central :(")
+        user_input = input("Qué desea hacer?\n")
+        if user_input == "1":
+            print("Adios - lease con voz de Walter")
+        else:
+            self.menu()
+node = CentralNode("127.0.0.1",9000)
+
+node.printNeighbors(node.neighbors)
